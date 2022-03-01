80da386aae3a3a53ddb7a399e16618d1b411c8ff
dEnis-ponti <delavaifag@gmail.com>
2022-03-02 02:13:31 +0300

    Swiper слайдер

---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+new Swiper('.reviews__slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' +
+      ' из ' +
+      '<span class="' + totalClass + '"></span>';
+    },
+  },
+  slidesPerView: 1,
+  slidesPreGroup: 1,
+  initialSlide: 1,
+  centeredSlides: true,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  grabCursor: true,
+});
