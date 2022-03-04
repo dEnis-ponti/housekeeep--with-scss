04c5d194c34ddbf5647769c790276d62b3c5a4ea
dEnis-ponti <delavaifag@gmail.com>
2022-03-04 15:54:10 +0300

    burger Для бедных

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,18 @@
 const openingBtn = document.querySelector('.burger-menu');
 const navMobile = document.querySelector('.header__nav-mobile');
+const burgerIcon = document.querySelector('.burger__icon');
 
 openingBtn.addEventListener('click', function () {
-  navMobile.classList.toggle("_active");
+  navMobile.classList.toggle("_active"); 
+
+  if (navMobile.classList.contains("_active")) {
+  console.log("YOI!");
+  burgerIcon.classList.toggle("_close") 
+  burgerIcon.setAttribute('src', '/img/icons/close.svg')
+  } else {
+    console.log("no!");
+    burgerIcon.classList.remove("_close")
+    burgerIcon.setAttribute('src', '/img/icons/burger.svg') 
+  }
 });
+
