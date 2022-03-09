feef4a95ce50e7413ca50052b8a2d31a6ce9e628
dEnis-ponti <delavaifag@gmail.com>
2022-03-09 07:12:18 +0300

    tablet Done

--- js/swiper.js
+++ js/swiper.js
@@ -12,11 +12,23 @@ new Swiper('.reviews__slider', {
       '<span class="' + totalClass + '"></span>';
     },
   },
-  slidesPerView: 3,
+  slidesPerView: 2,
+  spaceBetween: 25,
   slidesPreGroup: 1,
   initialSlide: 1,
-  centeredSlides: true,
-  spaceBetween: 30,
+  centeredSlides: false,
   slideToClickedSlide: true,
   grabCursor: true,
+  breakpoints: {
+    920 : {
+      centeredSlides: true,
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  },
 });
+
+// let activeSlide = document.querySelector('.swiper-slide-active + .swiper-slide');
+// console.log(activeSlide);
+// if()
+// activeSlide.classList.add('_active-tablet');
