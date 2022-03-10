5c5a5da7da3ff5601969bfd39a00f6c3f5f527ac
dEnis-ponti <delavaifag@gmail.com>
2022-03-11 00:22:53 +0300

    mobile_large ask-section, adaptive img resize

---
+++ #source/js/photo-adaptive.js
@@ -0,0 +1,26 @@
+console.log(document.documentElement.clientWidth);
+
+const diagnosticVarPicFirst = document.querySelector('.diagnostic-var__pic_first');
+const diagnosticVarPicSecond = document.querySelector('.diagnostic-var__pic_second');
+
+document.addEventListener("DOMContentLoaded", function(event)
+{
+    window.onresize = function() {
+        resize_info();
+    };
+});
+
+function resize_info()
+{
+    if (document.documentElement.clientWidth <= 575) {
+    diagnosticVarPicFirst.setAttribute('src', '/img/diagnostic-with-repair_mobile.png');
+    } else {
+      diagnosticVarPicFirst.setAttribute('src', '/img/diagnostic-with-repair.png');
+    }
+
+    if (document.documentElement.clientWidth <= 575) {
+      diagnosticVarPicSecond.setAttribute('src', '/img/diagnostic-without-repair_mobile.png');
+    } else {
+      diagnosticVarPicSecond.setAttribute('src', '/img/diagnostic-without-repair.png');
+    }
+}
